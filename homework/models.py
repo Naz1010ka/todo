57143f61e054230abf3c1509920215cc0efab90e
Naz1010ka <naziraisabaeva11@gmail.com>
2022-10-07 02:48:47 +0600

    added models tomeet

--- homework/models.py
+++ homework/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=100)
+    phone_number = models.IntegerField(default= True)
+    date_of_meeting = models.DateTimeField(auto_now_add = True)
+    comment = models.TextField(default="")
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
