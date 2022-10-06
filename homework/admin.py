16bdcc0d0c2cab59b7d10e01a3a645774c500923
Naz1010ka <naziraisabaeva11@gmail.com>
2022-10-07 03:15:19 +0600

    added models Goal_for_month

--- homework/admin.py
+++ homework/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import ToMeet
+from .models import ToMeet, Goal_for_month
 
 admin.site.register(ToMeet)
 
+admin.site.register(Goal_for_month)
--- homework/models.py
+++ homework/models.py
@@ -8,3 +8,10 @@ class ToMeet(models.Model):
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
 
+class Goal_for_month(models.Model):
+    Goal = models.CharField(max_length=100)
+    Month = models.DateTimeField(auto_now_add=True)
+    difficulty = models.FloatField(default="")
+    reason_for_goal = models.TextField(default="")
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
